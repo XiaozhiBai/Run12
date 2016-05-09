61b40743808964bd947a98c1a545a97986187517
Xiaozhi Bai <xiaozhi@uic.edu>
2016-05-09 11:51:54 -0700

    add adc0 cuts in thigger efficiency

--- StRoot/StEmbeddingAna/StTrigEfficiency.cxx
+++ StRoot/StEmbeddingAna/StTrigEfficiency.cxx
@@ -57,14 +57,14 @@ void StTrigEfficiency::read(TString fileName)
     {
       tracksMC->GetEntry(iTrk);
      
-      if(tracksMC->dca<1.5 && tracksMC->nfit>20 && tracksMC->nDedxPts>15 && tracksMC->nfit/(Float_t)tracksMC->nmax > 0.52 && sqrt(tracksMC->stpcx*tracksMC->stpcx+tracksMC->stpcy*tracksMC->stpcy)<73 && abs(tracksMC->eta)<0.7 && tracksMC->btowDsmAdc0 < tracksMC->btowAdc0*0.1 && 0<tracksMC->rpt  && tracksMC->bsmdNEta>1 && tracksMC->bsmdNPhi>1 && 0.3<tracksMC->p/tracksMC->btowE0 && tracksMC->p/tracksMC->btowE0<1.5 && abs(tracksMC->bemcDistZ)<3 && abs(tracksMC->bemcDistPhi)<0.015)
+      if(tracksMC->dca<1.5 && tracksMC->nfit>20 && tracksMC->nDedxPts>15 && tracksMC->nfit/(Float_t)tracksMC->nmax > 0.52 && sqrt(tracksMC->stpcx*tracksMC->stpcx+tracksMC->stpcy*tracksMC->stpcy)<73 && abs(tracksMC->eta)<0.7 && tracksMC->btowDsmAdc0 < tracksMC->btowAdc0*0.1 && 0<tracksMC->rpt  && tracksMC->bsmdNEta>1 && tracksMC->bsmdNPhi>1 && 0.3<tracksMC->p/tracksMC->btowE0 && tracksMC->p/tracksMC->btowE0<1.5 && abs(tracksMC->bemcDistZ)<3 && abs(tracksMC->bemcDistPhi)<0.015&&10<tracksMC->ncom)
 	{
 	  Float_t weight =Weight(tracksMC->pt,tracksMC->geantId);
 
 	  m_DSMadc->Fill(tracksMC->pt,weight);
-	  if(11<tracksMC->btowDsmAdc0 )
+	  if(11<tracksMC->btowDsmAdc0&&tracksMC->btowAdc0>180)
 	    m_DSMadc11_cut->Fill(tracksMC->pt,weight);
-	  if(18<tracksMC->btowDsmAdc0 )
+	  if(18<tracksMC->btowDsmAdc0 &&tracksMC->btowAdc0>300)
 	    m_DSMadc18_cut->Fill(tracksMC->pt,weight);	
 	}
     }
